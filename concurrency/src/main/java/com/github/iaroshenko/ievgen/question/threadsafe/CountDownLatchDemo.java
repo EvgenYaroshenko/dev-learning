da6e3025fa5fba89749128f3cf72bd103d2e10da
iiaroshenko <ievgen.iaroshenko@femiglobal.com>
2019-04-05 10:49:17 +0300

    add CountDownLatch

---
+++ concurrency/src/main/java/com/github/iaroshenko/ievgen/question/threadsafe/CountDownLatchDemo.java
@@ -0,0 +1,10 @@
+package com.github.iaroshenko.ievgen.question.threadsafe;
+
+/**
+ * Write class with distinct sets of active and passive threads,
+ * where we want to block the passive threads until all active threads give permission
+ * for the passive threads to procced
+ *
+ */
+public class CountDownLatchDemo {
+}
