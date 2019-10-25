8286518c7e3d50ec378a438d61ba59e3a51af763
iiaroshenko <ievgen.iaroshenko@femiglobal.com>
2019-10-25 20:11:56 +0300

    project euler template added

--- project-euler/src/main/java/com/github/iaroshenko/ievgen/Problem8.java
+++ project-euler/src/main/java/com/github/iaroshenko/ievgen/Problem8.java
@@ -1,6 +1,11 @@
 package com.github.iaroshenko.ievgen;
 
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
 //The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
 //
 //        73167176531330624919225119674426574742355349194934
@@ -26,11 +31,95 @@ package com.github.iaroshenko.ievgen;
 //
 //        Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
 public class Problem8 {
-    public long consecutiveProduct(String number, int i) {
-        if ()
 
+
+
+
+    @SuppressWarnings("Duplicates")
+    public static BigInteger consecutiveProduct(String number, int i) {
+        int cursor = 0;
+        int previousCursor = 0;
+        BigInteger result = BigInteger.ZERO;
+        BigInteger previousResult = BigInteger.ZERO;
+
+        if (!number.matches("[0-9]+")) {
+            throw new NumberFormatException("Invalid number");
+        }
+        if (i > number.length()) {
+            throw new NumberFormatException("i must be lesser or equal number length");
+        }
+
+        while (cursor <= number.length() - i) {
+            System.out.println(cursor);
+            boolean cursorBreaked = false;
+            for (int j = cursor; j < cursor + i; j++) {
+                if (number.charAt(j) == '0') {
+                    int curEl = j;
+                    while (curEl <= number.length() - i && number.charAt(curEl) == '0') {
+
+                            curEl = curEl + 1;
+                    }
+                    previousCursor = cursor;
+                    cursor = curEl + 1;
+                    cursorBreaked = true;
+                    break;
+                }
+            }
+
+            if (!cursorBreaked) {
+
+                if (previousResult.equals(BigInteger.ZERO)) {
+                    previousResult = BigInteger.ONE;
+                    for ( int j =  cursor; j < cursor + i; j++) {
+                        previousResult = previousResult.multiply(BigInteger.valueOf(number.charAt(j) - '0'));
+                    }
+                    if (previousResult.compareTo(result) == 1)
+                        result = previousResult;
+                    previousCursor = cursor;
+                    cursor = cursor + 1;
+                } else {
+                    if (cursor - previousCursor != 1) {
+
+                        previousResult = BigInteger.ONE;
+                        for ( int j =  cursor; j < cursor + i; j++) {
+                            previousResult = previousResult.multiply(BigInteger.valueOf(number.charAt(j) - '0'));
+                        }
+                        if (previousResult.compareTo(result) == 1)
+                            result = previousResult;
+                        previousCursor = cursor;
+                        cursor = cursor + 1;
+
+                    } else {
+                        if (number.charAt(cursor + i -1) == '0') {
+                            int tempValue = cursor + i - 1;
+                            while (tempValue < number.length() - i && number.charAt(tempValue) == '0')  {
+                                tempValue = tempValue + 1;
+                            }
+                            previousCursor = cursor;
+                            cursor = tempValue + 1;
+                        } else {
+                            BigInteger temp = previousResult;
+                            previousResult = temp.divide(BigInteger.valueOf(number.charAt(cursor-1) - '0')).multiply(BigInteger.valueOf(number.charAt(cursor + i - 1) - '0'));
+                            if (previousResult.compareTo(result) == 1)
+                                result = previousResult;
+                            previousCursor = cursor;
+                            cursor = cursor + 1;
+                        }
+
+                    }
+                }
+            }
+
+        }
+
+        return result;
     }
+
+
+
     public static void main(String[] args) {
 
+        String text = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
+        System.out.println(consecutiveProduct(text, 13));
     }
 }
